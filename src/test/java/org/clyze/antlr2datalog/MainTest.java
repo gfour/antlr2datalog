1edabfb78c5b593c81a31d3bc557101c2d4a082b
George Fourtounis <gfour@di.uoa.gr>
2021-03-01 14:24:13 +0200

    Improve Solidity syntax analysis.

--- src/test/java/org/clyze/antlr2datalog/MainTest.java
+++ src/test/java/org/clyze/antlr2datalog/MainTest.java
@@ -14,35 +14,6 @@ public class MainTest {
         FileUtils.deleteQuietly(new File(Main.DEFAULT_WORKSPACE));
     }
 
-    @Test public void testPython3() {
-        Main.main(new String[] { "-l", "python3", "-i", "grammars-v4/python/python3/examples/coroutines.py", "--compile" });
-        assert((new File(Main.DEFAULT_WORKSPACE, "database/BASE_FunctionDefinition.csv")).exists());
-    }
-
-    @Test public void testKotlin() {
-        Main.main(new String[] { "-l", "kotlin", "-i", "grammars-v4/kotlin/kotlin-formal/examples/Test.kt" });
-        assert((new File(Main.DEFAULT_WORKSPACE, "database/UsedVariable.csv")).exists());
-    }
-
-    @Test public void testCobol85() {
-        Main.main(new String[] { "-l", "cobol85", "-i", "src/test/resources/InputSORT.cbl" });
-        assert((new File(Main.DEFAULT_WORKSPACE, "database/BASE_StringConstant.csv")).exists());
-        assert((new File(Main.DEFAULT_WORKSPACE, "database/BASE_FunctionDefinition.csv")).exists());
-    }
-
-    @Test public void testLua() {
-        Main.main(new String[] { "-l", "lua", "-i", "grammars-v4/lua/examples/factorial.lua" });
-        assert((new File(Main.DEFAULT_WORKSPACE, "facts/DB_LUA_Var__NAME.facts")).exists());
-        assert((new File(Main.DEFAULT_WORKSPACE, "database/BASE_FunctionDefinition.csv")).exists());
-    }
-
-    @Test public void testRust() throws IOException {
-        String topSrcPath = (new File("grammars-v4/rust")).getCanonicalPath();
-        Main.main(new String[] { "-l", "rust", "-i", topSrcPath + "/examples/deno_core_runtime.rs", "--relative-path",  topSrcPath, "-g" });
-        assert((new File(Main.DEFAULT_WORKSPACE, "facts/DB_RUST_Identifier_NON_KEYWORD_IDENTIFIER.facts")).exists());
-        assertMetadataExist();
-    }
-
     @Test public void testC() {
         Main.main(new String[] { "-l", "c", "-i", "grammars-v4/c/examples/FuncForwardDeclaration.c", "-g" });
         assert((new File(Main.DEFAULT_WORKSPACE, "database/BASE_FunctionDefinition.csv")).exists());
@@ -54,10 +25,45 @@ public class MainTest {
         assert((new File(Main.DEFAULT_WORKSPACE, "database/BASE_FunctionDefinition.csv")).exists());
     }
 
+    @Test public void testCobol85() {
+        Main.main(new String[] { "-l", "cobol85", "-i", "src/test/resources/InputSORT.cbl" });
+        assert((new File(Main.DEFAULT_WORKSPACE, "database/BASE_StringConstant.csv")).exists());
+        assert((new File(Main.DEFAULT_WORKSPACE, "database/BASE_FunctionDefinition.csv")).exists());
+    }
+
     @Test public void testGo() {
         Main.main(new String[] { "-l", "go", "-i", "src/test/resources/bit_cmd.go", "-g" });
         assert((new File(Main.DEFAULT_WORKSPACE, "database/BASE_FunctionDefinition.csv")).exists());
         assert((new File(Main.DEFAULT_WORKSPACE, "database/BASE_Function_Area.csv")).exists());
+        assertMetadataExist();
+    }
+
+    @Test public void testKotlin() {
+        Main.main(new String[] { "-l", "kotlin", "-i", "grammars-v4/kotlin/kotlin-formal/examples/Test.kt" });
+        assert((new File(Main.DEFAULT_WORKSPACE, "database/UsedVariable.csv")).exists());
+    }
+
+    @Test public void testLua() {
+        Main.main(new String[] { "-l", "lua", "-i", "grammars-v4/lua/examples/factorial.lua" });
+        assert((new File(Main.DEFAULT_WORKSPACE, "facts/DB_LUA_Var__NAME.facts")).exists());
+        assert((new File(Main.DEFAULT_WORKSPACE, "database/BASE_FunctionDefinition.csv")).exists());
+    }
+
+    @Test public void testPython3() {
+        Main.main(new String[] { "-l", "python3", "-i", "grammars-v4/python/python3/examples/coroutines.py", "--compile" });
+        assert((new File(Main.DEFAULT_WORKSPACE, "database/BASE_FunctionDefinition.csv")).exists());
+    }
+
+    @Test public void testRust() throws IOException {
+        String topSrcPath = (new File("grammars-v4/rust")).getCanonicalPath();
+        Main.main(new String[] { "-l", "rust", "-i", topSrcPath + "/examples/deno_core_runtime.rs", "--relative-path",  topSrcPath, "-g" });
+        assert((new File(Main.DEFAULT_WORKSPACE, "facts/DB_RUST_Identifier_NON_KEYWORD_IDENTIFIER.facts")).exists());
+        assertMetadataExist();
+    }
+
+    @Test public void testSolidity() {
+        Main.main(new String[] { "-l", "solidity", "-i", "./grammars-v4/solidity/test.sol", "-g" });
+        assert((new File(Main.DEFAULT_WORKSPACE, "database/BASE_FunctionDefinition.csv")).exists());
         assertMetadataExist();
     }
 
