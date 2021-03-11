798177891a62d6ac3a08f677ffad5b4d52e73b85
George Fourtounis <gfour@di.uoa.gr>
2021-03-12 00:43:30 +0200

    Support Python function parameters.

--- src/test/java/org/clyze/antlr2datalog/MainTest.java
+++ src/test/java/org/clyze/antlr2datalog/MainTest.java
@@ -77,6 +77,9 @@ public class MainTest {
     @Test public void testPython3() {
         Main.main(new String[] { "-l", "python3", "-i", "grammars-v4/python/python3/examples/coroutines.py", "--compile" });
         assert((new File(Main.DEFAULT_WORKSPACE, "database/BASE_FunctionDefinition.csv")).exists());
+        assert functionDefinition("Funcdef@grammars-v4/python/python3/examples/coroutines.py@1048-1159", "coro", "grammars-v4/python/python3/examples/coroutines.py:209:12");
+        assert functionArity("Funcdef@grammars-v4/python/python3/examples/coroutines.py@1048-1159", "2");
+        assert variableDeclaration("TerminalNode@grammars-v4/python/python3/examples/coroutines.py@1052-1052", "args", "grammars-v4/python/python3/examples/coroutines.py:209:18");
     }
 
     @Test public void testRust() throws IOException {
