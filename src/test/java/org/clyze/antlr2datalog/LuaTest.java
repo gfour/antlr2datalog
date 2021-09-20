63e782880b6bef6deecf7092b6bc4411a036118f
George Fourtounis <gfour@di.uoa.gr>
2021-09-21 00:35:57 +0300

    Fix detection of Lua lambda parameters.

--- src/test/java/org/clyze/antlr2datalog/LuaTest.java
+++ src/test/java/org/clyze/antlr2datalog/LuaTest.java
@@ -11,8 +11,8 @@ public class LuaTest extends LangTest {
         Main.main(new String[] { "-l", "lua", "-i", SRC_FILE });
         assert((new File(Main.DEFAULT_WORKSPACE, "facts/DB_LUA_Var__NAME.facts")).exists());
         assert((new File(Main.DEFAULT_WORKSPACE, "database/BASE_FunctionDefinition.csv")).exists());
-        assert functionDefinition("Stat@grammars-v4/lua/examples/factorial.lua@1-24", "fact", "grammars-v4/lua/examples/factorial.lua:2:13");
-        assert functionArity("Stat@grammars-v4/lua/examples/factorial.lua@1-24", "1");
+        assert functionDefinition("Funcbody@grammars-v4/lua/examples/factorial.lua@3-24", "fact", "grammars-v4/lua/examples/factorial.lua:2:13");
+        assert functionArity("Funcbody@grammars-v4/lua/examples/factorial.lua@3-24", "1");
         assert variableDeclaration("TerminalNode@grammars-v4/lua/examples/factorial.lua@4-4", "n", "grammars-v4/lua/examples/factorial.lua:2:19");
         assert variableDeclaration("TerminalNode@grammars-v4/lua/examples/factorial.lua@29-29", "a", "grammars-v4/lua/examples/factorial.lua:11:4");
     }
