afcee44167335516d2c75ef66388b18da9451778
George Fourtounis <gfour@di.uoa.gr>
2021-09-21 11:17:15 +0300

    Recognize Go field declarations.

--- src/test/java/org/clyze/antlr2datalog/GoTest.java
+++ src/test/java/org/clyze/antlr2datalog/GoTest.java
@@ -9,14 +9,17 @@ public class GoTest extends LangTest {
     public static final String SRC_FILE = "src/test/resources/bit_cmd.go";
     @Test
     public void testGo() throws IOException {
-        Main.main(new String[] { "-l", "go", "-i", SRC_FILE, "-g", "-p" });
+        Main.main(new String[] { "-l", "go", "-i", SRC_FILE, "-i", "grammars-v4/golang/examples/structs.go", "-g", "-p" });
         assert((new File(Main.DEFAULT_WORKSPACE, "database/BASE_FunctionDefinition.csv")).exists());
         assert((new File(Main.DEFAULT_WORKSPACE, "database/BASE_Function_Area.csv")).exists());
         assert functionDefinition("FunctionDecl@src/test/resources/bit_cmd.go@1074-1544", "HijackGitCommandOccurred", "src/test/resources/bit_cmd.go:134:5");
         assert functionArity("FunctionDecl@src/test/resources/bit_cmd.go@1074-1544", "3");
         assert metadataExist();
         SourceMetadata sm = getSourceMetadata();
-        assert sm.functions.size() == 9;
-        assert sm.types.size() == 0;
+        assert sm.fields.size() == 14;
+        assert sm.functions.size() == 11;
+        assert sm.sourceFiles.size() == 2;
+        assert sm.types.size() == 10;
+        assert sm.variables.size() == 37;
     }
 }
